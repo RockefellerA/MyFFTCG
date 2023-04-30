fe9f85e9ca3d1fe7effe13ac37127204ce2543c4
RockefellerA <arkeband@hotmail.com>
2023-04-29 22:16:45 -0400

    remove hand currentHandSize attribute

--- fftcg/src/zone/Hand.java
+++ fftcg/src/zone/Hand.java
@@ -3,7 +3,5 @@ package zone;
 public class Hand extends Zone{
 	
 	public int MAX_HAND_SIZE = 5; // If you have over this number at end of turn, you must discard down to it.
-	
-	public int currentHandSize;
 
 }
